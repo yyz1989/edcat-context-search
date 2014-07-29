72d55cb85506d7cfd41ba80ffb41c7b6c68077be
Yang Yuanzhe <yyz1989@gmail.com>
2014-07-29 09:20:58 -0700

    Created methods to select datasets matching against given tags

--- src/main/java/eu/lod2/edcat/contextPlugin/ContextService.java
+++ src/main/java/eu/lod2/edcat/contextPlugin/ContextService.java
@@ -1,14 +1,12 @@
 package eu.lod2.edcat.contextPlugin;
 
+import eu.lod2.edcat.utils.DcatURI;
 import eu.lod2.edcat.utils.QueryResult;
 import eu.lod2.query.Db;
 import org.apache.commons.lang3.StringUtils;
 import org.openrdf.model.Model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by yyz on 7/29/14.
@@ -17,12 +15,12 @@ import java.util.Map;
 public class ContextService {
 
     public List<String> searchDatasets(Map<String[], String[]> searchCriteria, Map<String, String> context) {
-        String[] where = searchCriteriaToWhereClauses(searchCriteria, context);
-        List<String> datasets = sparqlSelect(where[0], where[1]);
+        String where = searchCriteriaToWhereClause(searchCriteria, context);
+        List<String> datasets = sparqlSelect(where);
         return datasets;
     }
 
-    private List<String> sparqlSelect(String whereIsDataset, String whereIsCatalog) {
+    private Vector<Map<String, String>> selectDatasetURIs(String where) {
         /**
          * The following SPARQL query contains two levels of queries: the subquery
          * collects all the datasets linked to the catalogs satisfying the constaints
@@ -32,74 +30,78 @@ public class ContextService {
          */
         QueryResult sparqlResults = Db.query("" +
                         " @PREFIX " +
-                        " SELECT DISTINCT ?dataset " +
+                        " SELECT ?dataset ?count" +
                         " WHERE {" +
-                        "    GRAPH ?dataset { " +
-                        "    ?dateset a dcat:Dataset ." +
-                        "    $whereIsDataset "+
+                        "    GRAPH ?ref { " +
+                        "    ?ref nif:sourceUrl ?dataset ."+
                         "    { "+
-                        "       SELECT DISTINCT ?dataset "+
+                        "       SELECT DISTINCT ?ref (COUNT(?tag) AS ?count) "+
                         "       WHERE {"+
-                        "         GRAPH ?catalog {"+
-                        "           ?catalog a dcat:Catalog . " +
-                        "           ?catalog dcat:dataset ?dataset . "+
-                        "           $whereIsCatalog "+
+                        "         GRAPH ?graph {"+
+                        "           ?segment nif:referenceContext ?ref. " +
+                        "           ?segment nif:anchorOf ?tag ."+
+                        "           $where "+
                         "         } "+
                         "       }" +
+                        "       GROUP BY ?ref "+
                         "    } "+
                         "  } "+
                         "}",
-                "whereIsDataset", whereIsDataset, "whereIsCatalog", whereIsCatalog);
-        List<String> datasets = new ArrayList<String>();
-        if (sparqlResults.size() > 0 )
-            for (Map<String, String> sparqlResult : sparqlResults)
-                datasets.add(sparqlResult.get("dataset"));
-        return datasets;
+                "where", where);
+        return sparqlResults;
     }
 
     /**
-     * Converts the given search criteria to two SPARQL where clauses for SPARQL queries performed on datasets and catalogs respectively
+     * Returns the datasets of a given catalog matching the given where clause
+     * @param where Where clause the datasets must comply to
+     * @param catalogId Id of the catalog to search datasets in
+     *
+     * @return  List of URIs of datasets matching the given where clause in the given catalog
+     */
+    private Map<String, String> selectDataset(String uri) {
+        QueryResult sparqlResults = Db.query("" +
+                        " @PREFIX" +
+                        " SELECT ?title ?description" +
+                        " WHERE {" +
+                        "   GRAPH $graph { " +
+                        "     $graph dct:title ?title."+
+                        "     $graph dct:description ?description."+
+                        "   }" +
+                        " }",
+                        "graph", uri);
+
+        Map<String, String> result = new HashMap<String, String>();
+        if (sparqlResults.size() > 0 ) result=sparqlResults.firstElement();
+        return result;
+    }
+
+    /**
+     * Converts the given search criteria to a SPARQL where clause that can be injected in a SPARQL query
      * @param searchCriteria  Map with search criteria mapping keywords to a value (e.g. [distribution][title] -> ["My title"])
      * @param context Context to convert the keywords to predicate URIs
      *
-     * @return A string array of SPARQL where clauses (actually two) to be injected in a query respectively
+     * @return A SPARQL where clause as String to inject in a SPARQL query
      */
-    private String[] searchCriteriaToWhereClauses(Map<String[], String[]> searchCriteria, Map<String, String> context) {
+    private String searchCriteriaToWhereClause(Map<String[], String[]> searchCriteria, Map<String, String> context) {
         int count = 0;
-        String whereIsCatalog = "";
-        String whereIsDataset = "";
+        String where = "";
+
         for (String[] keywords : searchCriteria.keySet()) {
-            ArrayList<String> predicates = new ArrayList<String>(Arrays.asList(keywordsToPredicates(keywords, context)));
-            ArrayList<String> values = new ArrayList<String>(Arrays.asList(searchCriteria.get(keywords)));
-            /** The constraints for datasets in a catalog search will always start with
-             *  the predicate dcat:dataset (e.g., search[dataset][theme][label]="wukwukfoo")
-             *  and therefore can be recognized and added into two where clauses
-             */
-            if(predicates.get(0).equals("<http://www.w3.org/ns/dcat#dataset>")) {
-                predicates.remove(0);
-                for (String value : values) {
-                    if (isSimpleStringValue(value)) {
-                        String tmpVar = "?" + count;
-                        whereIsDataset += String.format(" ?dataset %s %s .", StringUtils.join(predicates, "/"), tmpVar);
-                        whereIsDataset += String.format(" FILTER(str(%s)=%s)", tmpVar, value);
-                        count++;
-                    } else
-                        whereIsDataset += String.format(" ?dataset %s %s .", StringUtils.join(predicates, "/"), value);
-                }
-            }
-            else {
-                for (String value : values) {
-                    if (isSimpleStringValue(value)) {
-                        String tmpVar = "?" + count;
-                        whereIsCatalog += String.format(" ?catalog %s %s .", StringUtils.join(predicates, "/"), tmpVar);
-                        whereIsCatalog += String.format(" FILTER(str(%s)=%s)", tmpVar, value);
-                        count++;
-                    } else
-                        whereIsCatalog += String.format(" ?catalog %s %s .", StringUtils.join(predicates, "/"), value);
-                }
+            String[] predicates = keywordsToPredicates(keywords, context);
+            System.out.println(Arrays.toString(predicates));
+            String[] values = searchCriteria.get(keywords);
+            System.out.println(Arrays.toString(values));
+            for (String value : values) {
+                if (isSimpleStringValue(value)) {
+                    String tmpVar = "?" + count;
+                    where += String.format(" ?concept %s %s .", StringUtils.join(predicates, "/"), tmpVar);
+                    where += String.format(" FILTER(str(%s)=%s)", tmpVar, value);
+                    count++;
+                } else
+                    where += String.format(" ?concept %s %s .", StringUtils.join(predicates, "/"), value);
             }
         }
-        return new String[]{whereIsDataset,whereIsCatalog};
+        return where;
     }
 
     /**
