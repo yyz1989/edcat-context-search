11f7920b0aa3a1bc5ebcf043ef45200497581c01
Yang Yuanzhe <yuanzhe.yang@tenforce.com>
2014-07-30 07:42:58 -0700

    Created a test with Jersey rest client

--- src/test/java/eu/lod2/edcat/contextPlugin/test.java
+++ src/test/java/eu/lod2/edcat/contextPlugin/test.java
@@ -1,18 +1,36 @@
 package eu.lod2.edcat.contextPlugin;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by yyz on 7/30/14.
  */
 public class test {
     public static void main(String[] args) {
+        // catalog: http://tfvirt-lod2-dcat/catalogs/bbc29f1c-8876-41ef-8246-4fdf2fe49000/
+        // test dataset: http://tfvirt-lod2-dcat/catalogs/bbc29f1c-8876-41ef-8246-4fdf2fe49000/datasets/5ed51c6d-6809-43ba-a828-fd86e4d1a27a
+        // root segment: http://tfvirt-lod2-dcat/catalogs/bbc29f1c-8876-41ef-8246-4fdf2fe49000/datasets/d699274c-6be8-41c5-8e79-3eac3adb739c
+        // segment 1: http://tfvirt-lod2-dcat/catalogs/bbc29f1c-8876-41ef-8246-4fdf2fe49000/datasets/c1043aab-03ee-40a3-8061-8272a064837d
 
-        // URL: http://tfvirt-lod2-dcat/catalogs/bbc29f1c-8876-41ef-8246-4fdf2fe49000/
-        // catalog ID: bbc29f1c-8876-41ef-8246-4fdf2fe49000
-        // dataset 1: db2606b3-2c71-4d71-85c0-e5032d5941b6
-        // dataset 2: 3a42674c-21fd-4a6f-afad-3ce0e53eaa15
-        // dataset 3: 112f78d5-25b4-4192-8362-e8ff5f3a722f
-        // segment 1: 39c9b948-34d4-4078-a3fd-19373fcfc4cc
-        // segment 2: 2641d3f7-613a-4e96-a8dd-3ba2566cdb66
+        try {
+            String response= ClientBuilder.newClient()
+                    .target("http://localhost:8080/edcat/catalogs/context/test,test1,test2")
+                    .request(MediaType.APPLICATION_JSON)
+                    .get()
+                    .readEntity(String.class);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonArray array=new JsonParser().parse(response).getAsJsonArray();
+            System.out.println(gson.toJson(array));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         return;
     }
 }
